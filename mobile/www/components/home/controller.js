1b9589617f8410c4ba84a364bb530d62050852b8
teddest <tedd@Tedds-MacBook-Pro-2.local>
2016-10-25 12:48:18 -0400

    Add home view back-ground css and image.

--- mobile/www/components/home/controller.js
+++ mobile/www/components/home/controller.js
@@ -9,11 +9,7 @@
 
     function HomeController() {
         var vm = this;
-        vm.title = 'Home';
-
-        vm.labels2 = ['Entertainment', 'Medical', 'Membership', 'Fuel', 'Utilities', 'Travel', 'Groceries'];
-        vm.data2 = [6818.86, 7612.96, 10059.65, 3276.44, 4188.25, 10433.48, 14235.84 ];
-
+        vm.title = 'Welcome to Acme Bank';
     }
 
 })();
